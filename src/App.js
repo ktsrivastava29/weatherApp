e4252a508ea22d074145c578f5a56c6dab7b890e
Tanmay Srivastava <ktsrivastava29@gmail.com>
2021-08-14 18:21:07 +0530

    added react project

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
   }
 
   return (
-    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 21) ? 'app warm' : 'app') : 'app'}>
       <main>
         <div className="search-box">
           <input 
